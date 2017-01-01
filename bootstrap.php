d952d9d60303e57616c5fd948fed2fe84374c38d
Jakub Luczynski <jakub.luczynski@gmail.com>
2017-01-01 14:55:36 +0100

    PHPUnit config

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlbumTest;
+
+use Zend\Loader\AutoloaderFactory;
+use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\ServiceManager\ServiceManager;
+use RuntimeException;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+include __DIR__ . '/vendor/autoload.php';
