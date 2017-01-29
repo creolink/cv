8579c10a1cc94748722712907f5b40acd2f9ba4d
Jakub Luczynski <jakub.luczynski@gmail.com>
2017-01-29 12:59:36 +0100

    Refactoring-Employment-positions

--- module/Application/src/Element/AbstractEmployment.php
+++ module/Application/src/Element/AbstractEmployment.php
@@ -12,6 +12,13 @@ use Application\Config\PdfConfig;
 
 abstract class AbstractEmployment extends AbstractSection
 {
+    const SECTION_WIDTH = 197;
+    
+    /**
+     * @var float
+     */
+    private $urlWidth = 0;
+    
     protected function renderEmploymentPosition(
         $x,
         $y,
@@ -25,60 +32,18 @@ abstract class AbstractEmployment extends AbstractSection
         $contact = '',
         $companyUrl = ''
     ) {
-        $width = 197;
-        
         $this->renderDateAndCompany($x, $y, $date, $company);
-
-        $this->tcpdf->SetXY($x, $y += 4.5);        
-        $this->tcpdf->SetTextColor(90, 90, 90);
-        $this->tcpdf->SetFont($this->tcpdf->tahomaBold, '', 9);
-        $this->tcpdf->Cell(150, 6, $positionName, 0, 0, 'L', false);
-        
-        $this->tcpdf->SetTextColor(90, 90, 90);
-        $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 7);
-        
-        if (false === empty($references)) {
-            $this->tcpdf->SetXY($x, $y + 1.5);
-            $this->tcpdf->Cell(195, 2.2, 'References', '', 0, 'R', false, $references);
-            $this->tcpdf->Image(PdfConfig::PATH_IMAGES . 'save.png', $this->tcpdf->GetX(), $y + 1.5, 2.5, 2.5, 'PNG', $references);
-        }
-
-        if (false === empty($examples)) {
-            $shift = (false === empty($references) ? 17 : 0);
-            $this->tcpdf->SetXY($x, $y + 1.5);
-            $this->tcpdf->Cell(195 - $shift, 2.2, 'Examples', '', 0, 'R', false, $examples);
-            $this->tcpdf->Image(PdfConfig::PATH_IMAGES . 'save.png', $this->tcpdf->GetX(), $y + 1.5, 2.5, 2.5, 'PNG', $examples);
-        }
-        
-        $this->tcpdf->SetTextColor(90, 90, 90);
-        $this->tcpdf->SetXY($x + 1.5, $y += 5.5);
-        $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 8);
-        $this->tcpdf->MultiCell($width, 4, $description . "\r\n", 0, 'J', false);
-        
-        $this->tcpdf->SetTextColor(150, 150, 150);
-        
-        $y = $this->tcpdf->GetY() + 0.6;
-        
-        $urlWidth = 0;
-        if (false === empty($companyUrl)) {
-            $this->tcpdf->SetXY($x + 1, $y - 0.1);
-            $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 6.5);
-            $this->tcpdf->Cell($width, 2, $companyUrl, 0, 0, 'R', false, $companyUrl);
-            $urlWidth = (int)$this->tcpdf->GetStringWidth($companyUrl) + 0.3;
-        }
-
-        if (false === empty($contact)) {
-            $this->tcpdf->SetXY($x + 1, $y - 0.1);
-            $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 6.5);
-            $text = 'Contact: '
-                .$contact
-                .', '
-                .$address
-                .($urlWidth > 0 ? ', ' : '');
-            
-            $this->tcpdf->Cell($width - $urlWidth, 2, $text, 0, 0, 'R', false);
-        }
-        
+        $this->renderPositionName($x, $y, $positionName);
+        $this->renderReferences($x, $y, $references);
+        $this->renderExamples($x, $y, $examples, $references);
+        $this->renderDescription($x, $y, $description);
+        $this->renderCompanyUrl($x, $y, $companyUrl);
+        $this->renderContact($x, $y, $contact, $address);
+        $this->setCursor();
+    }
+    
+    private function setCursor()
+    {
         $this->tcpdf->cursorPositionY = $this->tcpdf->getY() + 4.3;
     }
     
@@ -88,5 +53,79 @@ abstract class AbstractEmployment extends AbstractSection
         $this->tcpdf->SetTextColor(90, 90, 90);
         $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 8);
         $this->tcpdf->Cell(50, 6, $date . ', ' . $company, 0, 0, 'L', false);
+    }
+    
+    private function renderPositionName($x, $y, $positionName)
+    {
+        $this->tcpdf->SetXY($x, $y += 4.5);        
+        $this->tcpdf->SetTextColor(90, 90, 90);
+        $this->tcpdf->SetFont($this->tcpdf->tahomaBold, '', 9);
+        $this->tcpdf->Cell(150, 6, $positionName, 0, 0, 'L', false);
+    }
+    
+    private function renderReferences($x, $y, $references)
+    {
+        $this->tcpdf->SetTextColor(90, 90, 90);
+        $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 7);
+        
+        if (false === empty($references)) {
+            $this->tcpdf->SetXY($x, $y + 1.5);
+            $this->tcpdf->Cell(195, 2.2, 'References', '', 0, 'R', false, $references);
+            $this->tcpdf->Image(PdfConfig::PATH_IMAGES . 'save.png', $this->tcpdf->GetX(), $y + 1.5, 2.5, 2.5, 'PNG', $references);
+        }
+    }
+    
+    private function renderExamples($x, $y, $examples, $references)
+    {
+        $this->tcpdf->SetTextColor(90, 90, 90);
+        $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 7);
+        
+        if (false === empty($examples)) {
+            $shift = (false === empty($references) ? 17 : 0);
+            $this->tcpdf->SetXY($x, $y + 1.5);
+            $this->tcpdf->Cell(195 - $shift, 2.2, 'Examples', '', 0, 'R', false, $examples);
+            $this->tcpdf->Image(PdfConfig::PATH_IMAGES . 'save.png', $this->tcpdf->GetX(), $y + 1.5, 2.5, 2.5, 'PNG', $examples);
+        }
+    }
+    
+    private function renderDescription($x, $y, $description)
+    {
+        $this->tcpdf->SetTextColor(90, 90, 90);
+        $this->tcpdf->SetXY($x + 1.5, $y += 5.5);
+        $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 8);
+        $this->tcpdf->MultiCell(self::SECTION_WIDTH, 4, $description . "\r\n", 0, 'J', false);
+    }
+    
+    private function renderCompanyUrl($x, $y, $companyUrl)
+    {
+        $this->tcpdf->SetTextColor(150, 150, 150);
+        
+        $y = $this->tcpdf->GetY() + 0.6;
+        
+        if (false === empty($companyUrl)) {
+            $this->tcpdf->SetXY($x + 1, $y - 0.1);
+            $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 6.5);
+            $this->tcpdf->Cell(self::SECTION_WIDTH, 2, $companyUrl, 0, 0, 'R', false, $companyUrl);
+            $this->urlWidth = $this->tcpdf->GetStringWidth($companyUrl) + 0.3;
+        }
+    }
+    
+    private function renderContact($x, $y, $contact, $address)
+    {
+        $this->tcpdf->SetTextColor(150, 150, 150);
+        
+        $y = $this->tcpdf->GetY() + 0.6;
+        
+        if (false === empty($contact)) {
+            $this->tcpdf->SetXY($x + 1, $y - 0.1);
+            $this->tcpdf->SetFont($this->tcpdf->tahoma, '', 6.5);
+            $text = 'Contact: '
+                .$contact
+                .', '
+                .$address
+                .($this->urlWidth > 0 ? ', ' : '');
+            
+            $this->tcpdf->Cell(self::SECTION_WIDTH - $this->urlWidth, 2, $text, 0, 0, 'R', false);
+        }
     }
 }
---
+++ module/Application/src/Entity/EmploymentPosition.php
@@ -0,0 +1,266 @@
+<?php
+/**
+ * @copyright 2015-2017 Jakub Luczynski
+ * @author Jakub Luczynski <jakub.luczynski@gmail.com>
+ * @link http://cv.creolink.pl/
+ */
+
+namespace Application\Entity;
+
+use Application\Entity\EntityInterface;
+
+class EmploymentPosition implements EntityInterface
+{
+    /**
+     * Name of position
+     * 
+     * @var string
+     */
+    private $name = '';
+    
+    /**
+     * Date of start work
+     * 
+     * @var string
+     */
+    private $dateStart;
+    
+    /**
+     * Date of finish work
+     * 
+     * @var string
+     */
+    private $dateEnd;
+    
+    /**
+     * Company name
+     * 
+     * @var string
+     */
+    private $company = '';
+    
+    /**
+     * Company address
+     * 
+     * @var string
+     */
+    private $address = '';
+    
+    /**
+     * Description of position
+     * 
+     * @var string
+     */
+    private $description = '';
+    
+    /**
+     * Examples url
+     * 
+     * @var string
+     */
+    private $examples = '';
+    
+    /**
+     * References url
+     * 
+     * @var string
+     */
+    private $references = '';
+    
+    /**
+     * Contact data
+     * 
+     * @var string
+     */
+    private $contact = '';
+    
+    /**
+     * Company url
+     * 
+     * @var string
+     */
+    private $companyUrl = '';
+
+    /**
+     * Sets position as disabled
+     * 
+     * @var bool
+     */
+    private $disabled = false;
+    
+    /**
+     * @param string $name
+     */
+    public function setName($name = '')
+    {
+        $this->name = $name;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @param string $dateStart
+     */
+    public function setDateStart($dateStart = '')
+    {
+        $this->dateStart = $dateStart;
+    }
+    
+    /**
+     * @return \DateTime
+     */
+    public function getDateStart()
+    {
+        return $this->dateStart;
+    }
+    
+    /**
+     * @param string $dateEnd
+     */
+    public function setDateEnd($dateEnd = '')
+    {
+        $this->dateEnd = $dateEnd;
+    }
+    
+    /**
+     * @return \DateTime
+     */
+    public function getDateEnd()
+    {
+        return $this->dateEnd;
+    }
+    
+    /**
+     * @param string $company
+     */
+    public function setCompany($company = '')
+    {
+        $this->company = $company;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+    
+    /**
+     * @param string $address
+     */
+    public function setAddress($address = '')
+    {
+        $this->address = $address;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+    
+    /**
+     * @param string $description
+     */
+    public function setDescription($description = '')
+    {
+        $this->description = $description;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    
+    /**
+     * @param string $examples
+     */
+    public function setExamples($examples = '')
+    {
+        $this->examples = $examples;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getExamples()
+    {
+        return $this->examples;
+    }
+    
+    /**
+     * @param string $references
+     */
+    public function setReferences($references = '')
+    {
+        $this->references = $references;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getReferences()
+    {
+        return $this->references;
+    }
+    
+    /**
+     * @param string $contact
+     */
+    public function setContact($contact = '')
+    {
+        $this->contact = $contact;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+    
+    /**
+     * @param string $companyUrl
+     */
+    public function setCompanyUrl($companyUrl = '')
+    {
+        $this->companyUrl = $companyUrl;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getCompanyUrl()
+    {
+        return $this->companyUrl;
+    }
+    
+    /**
+     * @param bool $disabled
+     */
+    public function setDisabled($disabled = false)
+    {
+        $this->disabled = $disabled;
+    }
+    
+    /**
+     * @return bool
+     */
+    public function isDisabled()
+    {
+        return $this->disabled;
+    }
+}
