12831afed988e169e57705a65ea08313de555157
Jakub Luczynski <jakub.luczynski@gmail.com>
2017-02-18 13:27:25 +0100

    Internationalization - translations step 1

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -47,4 +47,14 @@ return [
             ],
         ],
     ],
+    'translator' => [
+        'locale' => 'en_US',
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern' => '%s.mo',
+            ],
+        ],
+    ],
 ];
